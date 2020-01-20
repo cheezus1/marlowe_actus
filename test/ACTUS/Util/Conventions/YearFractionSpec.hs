b66556cd6303a525fdde3e9418a5c6068e22f42e
cheezus1 <bobinikolov33@gmail.com>
2020-01-20 11:43:17 +0200

    Test helper function

--- test/ACTUS/Util/Conventions/YearFractionSpec.hs
+++ test/ACTUS/Util/Conventions/YearFractionSpec.hs
@@ -3,9 +3,17 @@ module Util.Conventions.YearFractionSpec where
 import Test.Hspec
 import Control.Exception (evaluate)
 
+import Date.Time.Calendar
+
 spec :: Spec
 spec = do
   describe "Prelude.head" $ do
     it "throws an exception if used with an empty list" $ do
       evaluate (head [1]) `shouldThrow` anyException
       evaluate (head []) `shouldThrow` anyException
+
+timestampToDay timestamp =
+  let daysSince1970 = floor (timestamp / 86400)
+      day1970 = fromGregorian 1970 1 1
+  in
+    addDays daysSince1970 day1970
