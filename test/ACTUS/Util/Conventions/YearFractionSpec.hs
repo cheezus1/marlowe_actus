78a35d426ce64478bc6b9ddfafa1909c728b9bf3
cheezus1 <bobinikolov33@gmail.com>
2020-01-20 14:24:45 +0200

    Added tests for A/365 convention

--- test/ACTUS/Util/Conventions/YearFractionSpec.hs
+++ test/ACTUS/Util/Conventions/YearFractionSpec.hs
@@ -14,78 +14,157 @@ spec = do
     it "checks A/360 convention" $ do
       ((yearFraction A_360 (timestampToDay 1138665600)
           (timestampToDay 1141084800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.077777777777777777)
+        `shouldBe`
+        0.077777777777777777)
 
       ((yearFraction A_360 (timestampToDay 1138579200)
           (timestampToDay 1141084800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.080555555555555555)
+        `shouldBe`
+        0.080555555555555555)
 
       ((yearFraction A_360 (timestampToDay 1141084800)
           (timestampToDay 1141344000) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.008333333333333333)
+        `shouldBe`
+        0.008333333333333333)
 
       ((yearFraction A_360 (timestampToDay 1139875200)
           (timestampToDay 1141084800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.038888888888888888)
+        `shouldBe`
+        0.038888888888888888)
 
       ((yearFraction A_360 (timestampToDay 1159574400)
           (timestampToDay 1162252800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.086111111111111111)
+        `shouldBe`
+        0.086111111111111111)
 
       ((yearFraction A_360 (timestampToDay 1162252800)
           (timestampToDay 1164672000) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.077777777777777777)
+        `shouldBe`
+        0.077777777777777777)
 
       ((yearFraction A_360 (timestampToDay 1188518400)
           (timestampToDay 1204156800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.502777777777777777)
+        `shouldBe`
+        0.502777777777777777)
 
       ((yearFraction A_360 (timestampToDay 1204156800)
           (timestampToDay 1219881600) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.505555555555555555)
+        `shouldBe`
+        0.505555555555555555)
 
       ((yearFraction A_360 (timestampToDay 1204156800)
           (timestampToDay 1220054400) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.511111111111111111)
+        `shouldBe`
+        0.511111111111111111)
 
       ((yearFraction A_360 (timestampToDay 1204156800)
           (timestampToDay 1220140800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.513888888888888888)
+        `shouldBe`
+        0.513888888888888888)
 
       ((yearFraction A_360 (timestampToDay 1172448000)
           (timestampToDay 1204156800) (fromGregorian 1970 1 1))
-       `shouldBe`
-       1.019444444444444444)
+        `shouldBe`
+        1.019444444444444444)
 
       ((yearFraction A_360 (timestampToDay 1172448000)
           (timestampToDay 1204243200) (fromGregorian 1970 1 1))
-       `shouldBe`
-       1.022222222222222222)
+        `shouldBe`
+        1.022222222222222222)
 
       ((yearFraction A_360 (timestampToDay 1204243200)
           (timestampToDay 1235779200) (fromGregorian 1970 1 1))
-       `shouldBe`
-       1.013888888888888888)
+        `shouldBe`
+        1.013888888888888888)
 
       ((yearFraction A_360 (timestampToDay 1204156800)
           (timestampToDay 1206835200) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.086111111111111111)
+        `shouldBe`
+        0.086111111111111111)
 
       ((yearFraction A_360 (timestampToDay 1204156800)
           (timestampToDay 1206921600) (fromGregorian 1970 1 1))
-       `shouldBe`
-       0.088888888888888888)
+        `shouldBe`
+        0.088888888888888888)
+
+    it "checks A/365 convention" $ do
+      ((yearFraction A_365 (timestampToDay 1138665600)
+          (timestampToDay 1141084800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.076712328767123287)
+
+      -- precision mismatch ?
+      -- expected: 7.945205479452054e-2
+      -- but got: 7.945205479452055e-2
+      ((yearFraction A_365 (timestampToDay 1138579200)
+          (timestampToDay 1141084800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.079452054794520550)
+
+      ((yearFraction A_365 (timestampToDay 1141084800)
+          (timestampToDay 1141344000) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.00821917808219178)
+
+      ((yearFraction A_365 (timestampToDay 1139875200)
+          (timestampToDay 1141084800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.038356164383561643)
+
+      ((yearFraction A_365 (timestampToDay 1159574400)
+          (timestampToDay 1162252800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.084931506849315068)
+
+      ((yearFraction A_365 (timestampToDay 1162252800)
+          (timestampToDay 1164672000) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.076712328767123287)
+
+      ((yearFraction A_365 (timestampToDay 1188518400)
+          (timestampToDay 1204156800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.495890410958904109)
+
+      ((yearFraction A_365 (timestampToDay 1204156800)
+          (timestampToDay 1219881600) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.498630136986301369)
+
+      ((yearFraction A_365 (timestampToDay 1204156800)
+          (timestampToDay 1220054400) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.504109589041095890)
+
+      ((yearFraction A_365 (timestampToDay 1204156800)
+          (timestampToDay 1220140800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.506849315068493150)
+
+      ((yearFraction A_365 (timestampToDay 1172448000)
+          (timestampToDay 1204156800) (fromGregorian 1970 1 1))
+        `shouldBe`
+        1.005479452054794520)
+
+      ((yearFraction A_365 (timestampToDay 1172448000)
+          (timestampToDay 1204243200) (fromGregorian 1970 1 1))
+        `shouldBe`
+        1.008219178082191780)
+
+      ((yearFraction A_365 (timestampToDay 1204243200)
+          (timestampToDay 1235779200) (fromGregorian 1970 1 1))
+        `shouldBe`
+        1.0)
+
+      ((yearFraction A_365 (timestampToDay 1204156800)
+          (timestampToDay 1206835200) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.084931506849315068)
+
+      ((yearFraction A_365 (timestampToDay 1204156800)
+          (timestampToDay 1206921600) (fromGregorian 1970 1 1))
+        `shouldBe`
+        0.087671232876712328)
 
     it "checks 30E/360 convention" $ do
       ((yearFraction E30_360 (timestampToDay 1138665600)
